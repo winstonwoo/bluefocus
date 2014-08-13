6f8bdabf63f61f063d54cdbe55401119ed000760
root <root@winston-Vostro-270.(none)>
2014-08-13 13:59:03 +0800

    MY_LOG

--- app/triggerApp.c
+++ app/triggerApp.c
@@ -10,6 +10,12 @@
 #define CMD_BEGIN 0xab
 #define LENGTH 30
 
+#ifdef MY_LOG
+#define myprintf printf
+#else
+#define myprintf 
+#endif
+
 int psdata = 0xff;
 
 int*  change(char *strg)
@@ -34,7 +40,7 @@ int*  change(char *strg)
             for (j = i + 1; strg[j] && strg[j] != ' '; ++j);
             number[++n] = atoi(strg + i);
             psdata = number[n] ;
-            printf("!!!!!0x%x ####%d\n", number[n], n );
+            myprintf("!!!!!0x%x ####%d\n", number[n], n );
             i = j;
       }
       number[0] = n;
@@ -54,10 +60,10 @@ int system_data(void){
       len = read(fd, str, LENGTH) ;
     }
     str[len] = '\0' ;
-    printf("!%s\n", str) ;
+    myprintf("!%s\n", str) ;
 
     p = change(str);
-    printf("num1 = %d, %d, %d\n", num[0], num[1], num[2]) ;
+    myprintf("num1 = %d, %d, %d\n", num[0], num[1], num[2]) ;
     close(fd) ;
     return len ;
 }
@@ -71,25 +77,25 @@ int sub_main()
     status = system("pgrep omxplayer > system.dat");
 
     system_data() ;
-    printf("wifexited(status):%d/n", WIFEXITED(status));
-    printf("WEXITSTATUS(status):%d/n", WEXITSTATUS(status));
-    printf("status = 0x%x \n", status) ;
+    myprintf("wifexited(status):%d/n", WIFEXITED(status));
+    myprintf("WEXITSTATUS(status):%d/n", WEXITSTATUS(status));
+    myprintf("status = 0x%x \n", status) ;
     if (status == -1) {
-        printf("system error!");
+        myprintf("system error!");
     }
 
 
     if (WIFEXITED(status)) {
-        printf("cp exit normal![%d]/n", errno);
-        printf("exit staus = [%X]/n", WEXITSTATUS(status));
+        myprintf("cp exit normal![%d]/n", errno);
+        myprintf("exit staus = [%X]/n", WEXITSTATUS(status));
     } else
-        printf("cp exit illegal![%d]/n", errno);
+        myprintf("cp exit illegal![%d]/n", errno);
 
     if(psdata != 0xff)
 	{
     sprintf(cmdStr, "kill %d", psdata+1) ;
 
-    printf("$$$ths cmd %s\n", cmdStr) ;
+    myprintf("$$$ths cmd %s\n", cmdStr) ;
     system(cmdStr) ;
      }
 }
@@ -111,18 +117,18 @@ int main(){
 
   ioctl(fd, CMD_BEGIN, 0xaa);
 
-  printf("send command CMD_BEGIN with 0xaa parameter\n") ;
+  myprintf("send command CMD_BEGIN with 0xaa parameter\n") ;
  
   while(1){
 
     read(fd, &val, sizeof(char));
-    printf("val = %x\n", val) ;
+    myprintf("val = %x\n", val) ;
 
     switch(val){
     case 0x17:
          sub_main() ;
 	 if(fork() == 0){
-         printf("I'm the child process. \n") ;
+         myprintf("I'm the child process. \n") ;
          status = system("omxplayer -o hdmi mus.mp4") ;  
          exit(0) ;
          }
@@ -131,7 +137,7 @@ int main(){
     case 0x18:
         sub_main() ;
     	if(fork() == 0){
-         printf("I'm the child process. \n") ;
+         myprintf("I'm the child process. \n") ;
          status = system("omxplayer -o hdmi pho.mp4") ;  
          exit(0) ;
          }       
@@ -141,7 +147,7 @@ int main(){
     case 0x22:
         sub_main() ;
   	if(fork() == 0){
-         printf("I'm the child process. \n") ;
+         myprintf("I'm the child process. \n") ;
          status = system("omxplayer -o hdmi sig.mp4") ;  
          exit(0) ;
          } 
@@ -151,25 +157,25 @@ int main(){
     case 0x23:
         sub_main() ;	
 	if(fork() == 0){
-         printf("I'm the child process. \n") ;
+         myprintf("I'm the child process. \n") ;
          status = system("omxplayer -o hdmi xos.mp4") ;  
          exit(0) ;
          }
          //status = system("omxplayer -o hdmi xos.mp4") ;  
         break ;
     default:
-        printf("No trigger occur!\n") ;
+        myprintf("No trigger occur!\n") ;
 
     }
 #if 0
     if(val == 0x17){
         if(fork() == 0){
-         printf("I'm the child process. \n") ;
+         myprintf("I'm the child process. \n") ;
          status = system("omxplayer -o hdmi iphone.mp4") ;  
          close(fd) ;
          exit(0) ;
          }else{
-           printf("I'm the parent. \n") ;
+           myprintf("I'm the parent. \n") ;
          }
     }
 #endif
